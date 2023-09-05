db221e66134040e6173dc4630def50358741bc50
Juhi Bhojani <89679280+Juhibhojani@users.noreply.github.com>
2023-09-05 18:40:40 +0530

    Update Zomato.py

--- Scrapers/Zomato.py
+++ Scrapers/Zomato.py
@@ -1,41 +1,59 @@
 import re
 import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-# Set up the Chrome webdriver
-driver = webdriver.Chrome()
+def scroll_to_bottom(driver):
+    # Scroll to the bottom of the page
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)  # Adjust the wait time based on your network speed and content loading speed
 
-url = "https://zomato.com/ahmedabad/restaurants/cafes?category=2"
-driver.get(url)
-
-# Wait for the page to load
-time.sleep(5)
-
-# Get the HTML of the page after waiting for some time
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-
-container = soup.find("div", {"id": "root"})
-
-# Loop through the containers to extract restaurant information
-while True:
-    i = 0
+def parse_and_print_items(container,city):
+    data = []
     for items in container.find_all("div", class_=re.compile("sc-1mo3ldo-0 sc-")):
-        if i == 0:
-            i = 1
-            continue
-        print(items.text)
-        first_child = items.find("div")
-        for item in first_child:
-            link = item.find("a", href=True)['href']
-            print(link)
-            name = item.find("h4")
-            print(name.text)
-            rating = item.find("div", {"class": "sc-1q7bklc-1 cILgox"})
-            print(rating.text)
-            cusine = item.find("p")
-            print(cusine.text)
-            rate = item.find("p").next_sibling
-            print(rate.text)
+        if items.find("h4"):
+            link = "https://www.zomato.com" + items.find("a", href=True)['href']
+            print("Link:", link)
+            name = items.find("h4").text
+            print("Name:", name)
+            rating = items.find("div", {"class": "sc-1q7bklc-1 cILgox"}).text
+            print("Rating:", rating)
+            cuisine = items.find("p").text
+            print("Cuisine:", cuisine)
+            rate = items.find("p").next_sibling.text
+            print("Rate:", rate)
+            print("-" * 50)
+            print(city)
+            li = [name,rating,cuisine,rate,link,city]
+            data.append(li)
+    return data
+
+
+
+def infinite_scroll(url,city):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    x = []
+    try:
+        while True:
+            scroll_to_bottom(driver)
+            html = driver.page_source
+            soup = BeautifulSoup(html, "html.parser")
+            container = soup.find("div", {"id": "root"})
+            x += parse_and_print_items(container,city)
+    except:
+        print("Scraping interrupted by the user.")
+        return x
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    x = []
+    cites = ["ahmedabad","mumbai","pune","bangalore","udaipur","jaipur","surat","indore","chennai","chandigarh"]
+    for city in cites:
+        url_to_scrape = f"https://zomato.com/{city}/restaurants/cafes?category=2"
+        x += infinite_scroll(url_to_scrape,city)
+    df = pd.DataFrame(x)
+    df.to_csv(r"zomato.csv")
